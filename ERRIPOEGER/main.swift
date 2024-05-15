27fc76c86e2fb033c9a7f0bb195812fb8bf09e28
triciamilet <miletpati@gmail.com>
2024-05-15 17:44:14 -0300

    All the story until the first path choices is programmed. Both mission board and tower ASCII is done.

--- ERRIPOEGER/main.swift
+++ ERRIPOEGER/main.swift
@@ -8,37 +8,86 @@
 import Foundation
 
 slowPrint(text: "No mundo de Wald, a única forma de sobrevivência é arriscando a sua própria vida. Os mais ricos da sociedade pagam qualquer um para fazer o que eles quiserem, e eles sempre querem o pior, pedem coisas que eles próprios não tem coragem de fazer.")
-print(" ▼")
-readLine()
 slowPrint(text: "Você é o agente Coelho, um dos milhares caçadores de recompensa. Após terminar a sua missão mais recente, você foi até o painel de solicitações para aceitar a próxima.")
-print(" ▼")
-readLine()
-slowPrint(text: "No painel, tem uma missão de alto risco. O objetivo é recuperar uma joia valiosa que está localizada em uma tumba no deserto de Skarlet. O risco é alto, mas a recompensa é maior ainda.")
-print(" ▼")
-readLine()
 
 print("""
-        ┏━━━━━━┓                       ┏━━━━━━┓
-┏━━━━━━━┻━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━┻━━━━━┓
-┃    __  __ ___ ____ ____   ___  _____ ____         ┃
-┃    |  ╲╱  |_ _/ ___/ ___| / _ \| ____/ ___|       ┃
-┃    | |\╱| || |\___ \___ \┃ | | |  _| \___ ╲       ┃
-┃    | |  | || | ___) |__) | |_| | |___ ___) |      ┃
-┃    |_|  |_|___|____/____/ \___/|_____|____/       ┃
-┃                                                   ┃
-┃       ┏━━━━━┓                     ┏━━━━━┓         ┃
-┃   ┏━━━┻━┓   ┃  ┏━━━━━┓            ┃     ┃         ┃
-┃   ┃     ┃   ┃  ┃    ┏┻━━━━┓       ┃     ┃         ┃
-┃   ┃     ┃   ┃  ┃┏━━━┻━┓   ┃       ┃     ┣━━━━━┓   ┃
-┃   ┃    ┏┻━━━━┓ ┃┃     ┃   ┃       ┗━━━━━┫     ┃   ┃
-┃   ┗━━━━┫     ┃ ┗┃     ┃   ┃             ┃     ┃   ┃
-┃        ┃     ┃  ┃     ┣━━━┛             ┃     ┃   ┃
-┃        ┃     ┃  ┗━━━━━┛                 ┗━━━━━┛   ┃
-┃        ┗━━━━━┛                                    ┃
-┗━━━━━━┳━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━┳━━━━━━┛
-       ┃      ┃                       ┃      ┃
-       ┊      ┊                       ┊      ┊
+               ┏━━━━━━┓                       ┏━━━━━━┓
+        ┏━━━━━━┻━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━┻━━━━━━┻━━━━━━┓
+        ┃    __  __ ___ ____ ____   ___  _____ ____         ┃
+        ┃    |  ╲╱  |_ _/ ___/ ___| / _ ╲| ____/ ___|       ┃
+        ┃    | |╲╱| || |╲___ ╲___ ╲┃ | | |  _| ╲___ ╲       ┃
+        ┃    | |  | || | ___) |__) | |_| | |___ ___) |      ┃
+        ┃    |_|  |_|___|____/____/ ╲___/|_____|____/       ┃
+        ┃                                                   ┃
+        ┃       ┏━━━━━┓                     ┏━━━━━┓         ┃
+        ┃   ┏━━━┻━┓   ┃  ┏━━━━━┓            ┃     ┃         ┃
+        ┃   ┃     ┃   ┃  ┃    ┏┻━━━━┓       ┃     ┃         ┃
+        ┃   ┃     ┃   ┃  ┃┏━━━┻━┓   ┃       ┃     ┣━━━━━┓   ┃
+        ┃   ┃    ┏┻━━━┻┓ ┃┃ 👑✱ ┃   ┃       ┗━━━━━┫     ┃   ┃
+        ┃   ┗━━━━┫     ┃ ┗┫ $99 ┃   ┃             ┃     ┃   ┃
+        ┃        ┃     ┃  ┃ 999 ┣━━━┛             ┃     ┃   ┃
+        ┃        ┃     ┃  ┗━━━━━┛                 ┗━━━━━┛   ┃
+        ┃        ┗━━━━━┛                                    ┃
+        ┗━━━━━━┳━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━┳━━━━━━┛
+               ┃      ┃                       ┃      ┃
+               ┊      ┊                       ┊      ┊
 """)
+print()
+slowPrint(text: "No painel, tem uma missão de alto risco. O objetivo é recuperar uma joia valiosa que está localizada em uma tumba no deserto de Skarlet. O risco é alto, mas a recompensa é maior ainda.")
+
+slowPrint(text: "A viagem até as tumbas de Skarlet é sufocante. O sol escaldante e  o horizonte interminável lhe cansam, mas não são suficientes para te fazer desistir. Você anda por mais de 5 quilômetros sem parar até chegar no seu destino final.")
+
+print("""
+
+
+                _
+              (`  ).                   _
+             (     ).              .:(`  )`.
+            _(       '`.          :(   .    )
+        .=(`(      .   )    .--   `.  (    ) )
+       ((    (..__.:'-'  .+(   )   ` _`  ) )
+       `(       ) )      (   .  )    (   )  ._
+        ` __.:'   )    (   (   ))   `-'.-(`  )
+    ( )       --'      `- __.'        :(      ))
+(_.'   ┃       .')      ┃            ┃  `(    )  ))
+       ┃          (_  ) ┃            ┃    ` __.:'
+       ┃                ┃            ┃
+       ┃                ┃            ┃
+       ┃                ┃            ┃
+       ┃                ┃            ┃
+       ┃                ┃            ┃
+       ┃                ┃            ┃
+       ┃                ┃            ┃
+      ┃                 ┃             ┃
+      ┃                 ┃             ┃
+      ┃                 ┃             ┃
+      ┃                 ┃             ┃
+      ┃                 ┃             ┃
+      ┃                 ┃             ┃
+      ┃                 ┃             ┃
+      ┃                 ┃             ┃
+     ┃                  ┃              ┃
+     ┃                  ┃              ┃
+     ┃                  ┃              ┃
+     ┃                  ┃              ┃
+     ┃                  ┃              ┃
+     ┃                  ┃              ┃
+     ┃                  ┃              ┃
+    ┃                   ┃               ┃
+    ┃                   ┃               ┃
+    ┃  🔥🔥 🔥 🔥 🔥    ┃   🔥      🔥  ┃
+    ┃  ┃  ┃  ┃  ┃  ┃    ┃   ┃        ┃  ┃
+    ┃  ┃  ┃  ┃  ┃  ┃    ┃   ┃        ┃  ┃
+    ┃                   ┃               ┃    ()()
+    ┗━━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━┛    (*  )o
+    
+""")
+
+slowPrint(text: "A construção é monumental e quase que etérea, você não consegue imaginar como que isso tudo poderia teer sido construído por seres vivos mortais. Você olha para cima para ver a imensidão e o prédio some em meio às nuvens.")
+
+slowPrint(text: "Você anda em volta e analisa as paredes, parece que não há portas. Mas deve haver um meio de entrar.")
+
+
 
 //Game's func
 func slowPrint(text: String) {
@@ -48,4 +97,6 @@ func slowPrint(text: String) {
         print(word, terminator: " ")
         usleep(100000)
     }
+    print(" ▼")
+    readLine()
 }
